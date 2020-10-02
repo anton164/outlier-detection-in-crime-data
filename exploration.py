d91006e8e912498616d5ef18aa6d4aa226a9c218
Anton Abilov <anton.abilov@gmail.com>
2020-10-02 19:28:44 -0400

    Updates

--- exploration.py
+++ exploration.py
@@ -1,13 +1,33 @@
 import streamlit as st
 import pandas as pd
 
+def filter_by_location (df, location):
+    return df.loc[df['BORO_NM'] == location]
+
+def print_dataframe (df):
+    st.markdown("Data shape:")
+    st.write(df.shape)
+    st.markdown("First 10 rows:")
+    st.dataframe(df.head(10))
+
+st.markdown("""
+# Evaluating Methods for Outlier Detection in NYC Crime Incidents
+""")
+
 raw_data = pd.read_csv("./NYPD_Complaint_Data.csv")
 
-st.markdown("Data shape:")
-st.write(raw_data.shape)
-
-st.markdown("First 10 rows:")
-st.dataframe(raw_data.head(10))
+print_dataframe(raw_data)
 
 st.code("pandas.describe()")
 st.write(raw_data.describe(include='all'))
+
+st.markdown("""
+### Crime in Brooklyn
+""")
+
+brooklyn_crimes = filter_by_location(raw_data, 'BROOKLYN')
+
+print_dataframe(brooklyn_crimes)
+
+st.markdown("""
+""")
