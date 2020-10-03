628425422e4b7b1ab2666d657d19b05dc9e2f6e5
Anton Abilov <anton.abilov@gmail.com>
2020-10-02 20:00:20 -0400

    Added chart for plotting number of complaints over time

--- exploration.py
+++ exploration.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+
+def parse_data(raw_data):
+    raw_data['parsed_time'] = pd.to_datetime(raw_data['CMPLNT_FR_DT'], errors='coerce')
+    parsed_data = raw_data.set_index('parsed_time')
+    return parsed_data[parsed_data.index.year == 2020]
 
 def filter_by_location (df, location):
     return df.loc[df['BORO_NM'] == location]
@@ -21,13 +27,25 @@ print_dataframe(raw_data)
 st.code("pandas.describe()")
 st.write(raw_data.describe(include='all'))
 
-st.markdown("""
-### Crime in Brooklyn
-""")
-
-brooklyn_crimes = filter_by_location(raw_data, 'BROOKLYN')
-
-print_dataframe(brooklyn_crimes)
+parsed_data = parse_data(raw_data)
 
 st.markdown("""
+### Filter Crimes by Location
 """)
+
+location = st.selectbox("Select location", parsed_data['BORO_NM'].unique())
+location_crimes = filter_by_location(parsed_data, location)
+
+counts = location_crimes.groupby(['parsed_time']).agg(len)
+
+fig = px.line(counts, y="CMPLNT_NUM", labels=["Test", "be"])
+fig.update_xaxes(title="Time of complaint")
+fig.update_yaxes(title="Number of complaints")
+st.plotly_chart(fig)
+
+st.markdown('#### Crimes in {}'.format(location))
+print_dataframe(location_crimes)
+
+st.markdown("""
+""")
+
